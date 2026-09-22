6f3fec651537188d48f20e36858284871b882b0f
janjirlashireesha84 <janjirlashireesha@gmail.com>
2026-09-22 21:35:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message
+console.log("Welcome to Shireesha's Portfolio!");
+
+// Button interaction
+const button = document.querySelector(".button");
+
+button.addEventListener("click", function () {
+    console.log("Thanks for visiting my portfolio!");
+});
